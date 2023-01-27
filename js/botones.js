9527226b57335bcba677aa009fcf2fd63d93790d
Fernán en Manjaro <ferpanz@gmail.com>
2023-01-26 21:28:53 -0300

    listo

--- js/botones.js
+++
@@ -1,4 +0,0 @@
-/** Pop-up de confirmación de la página */
-document.addEventListener('boton').onclick = windows.alert () {
-    document.getElementById('boton').onclick = window.alert("Estás por salir de la página")
-}
