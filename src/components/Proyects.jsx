4637afa6183ca685fa8f87c26a33941827f00ba3
Fernán <ferpanz@gmail.com>
2025-09-10 16:56:45 -0300

    titulo: "Actualización de proyectos y estilos",

--- src/components/Proyects.jsx
+++ src/components/Proyects.jsx
@@ -4,11 +4,14 @@ import ProyectCard from '../subcomponents/ProyectCard'
 
 const Proyects = () => {
   return (
+    <>
+    <h2 className='text-2xl font-bold mb-4 mt-3 text-center text-white'>Proyectos</h2>
     <div className="d-flex flex-wrap justify-content-center">
       {proyectos.map((proyecto, idx) => (
         <ProyectCard key={idx} {...proyecto} />
       ))}
     </div>
+    </>
   )
 }
 
